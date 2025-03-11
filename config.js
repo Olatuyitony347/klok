d28853e79b71aeb3bd3322c1f355d0003d01dde0
Olatuyi Tony <86169011+Olatuyitony347@users.noreply.github.com>
2025-03-11 19:25:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   API_BASE_URL: "https://api1-pp.klokapp.ai/v1",
   MAX_FAILED_ATTEMPTS: 3,
   DELAY_CHAT: [10, 30], //seconds, min: 10s
-  REF_CODE: "Q6A4KKEJ", //referral code
+  REF_CODE: "47278ZKW", //referral code
   AMOUNT_REF: 20, //number of ref to buff
   DELAY_START_BOT: [1, 15],
   DELAY_REQUEST_API: [5, 15],
